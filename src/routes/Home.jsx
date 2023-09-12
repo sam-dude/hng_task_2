474b9829a885bb7c47c2d8396dd1a020a2964a08
sam-dude <blendert5@gmail.com>
2023-09-12 17:11:02 +0100

    add

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -15,15 +15,18 @@ export default function Home() {
 
         {/* featured section */}
         <section className='min-h-screen px-[8%] '>
-          <div className='flex justify-between w-full'>
-            <h3 className='font-semibold text-4xl my-4'>Featured Movies</h3>
+          <div className='flex justify-between w-full items-end'>
+            <h3 className='font-semibold text-3xl my-4 md:text-4xl'>Featured Movies</h3>
             <div className='text-red-500'>See more</div>
           </div>
 
           {/* List of movies */}
-          <div className='grid md:grid-cols-4 grid-cols-2 gap-4'>
-            <div className='h-full'>
-              <img src='' alt='movie-poster' className='h-full'/>
+          <div className='grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4'>
+            <div className=''>
+              <img src='' alt='movie-poster' className=' w-full'/>
+              <div>USA 2016 - current</div>
+              <div>Stranger Things</div>
+              <div>Action, Horror, Adventure</div>
             </div>
             <div>me</div>
             <div>me</div>
