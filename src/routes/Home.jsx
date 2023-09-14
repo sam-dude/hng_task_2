370885deaa57fbfbbb5afbe49b8e55e65fbf3bae
sam-dude <blendert5@gmail.com>
2023-09-15 00:24:19 +0100

    fix

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -63,16 +63,17 @@ export default function Home() {
 
             {
               movies.slice(0, 10).map((movie, i) => (
-                <Link to={`/movies/${movie.id}`} className='relative' key={i} data-testid='movie-card'>
+                <div className="relative" data-testid='movie-card' key={i}>
+                <Link to={`/movies/${movie.id}`}  >
                   <img data-testid="movie-poster" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt='movie-poster' className=' w-full'/>
                   <div className="text-sm">USA <span data-testid="movie-release-date">{movie.release_date}</span></div>
                   <div className='font-bold' data-testid='movie-title'>{movie.title}</div>
                   <div className="text-sm">Action, Horror, Adventure</div>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 absolute top-4 right-4 text-gray-200 bg-gray-50 bg-opacity-50 p-2 rounded-full">
-                    <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-                  </svg>
-
                 </Link>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 absolute top-4 right-4 text-gray-200 bg-gray-50 z-10 bg-opacity-50 p-2 rounded-full">
+                  <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                </svg>
+                </div>
               ))
             }
             
