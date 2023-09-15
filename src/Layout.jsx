c9f032bcf5c8fde53a81ea7b011c339485d1416a
sam-dude <blendert5@gmail.com>
2023-09-15 20:30:57 +0100

    fix

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,13 +7,13 @@ export default function Layout() {
         
         <Outlet />
         {/* Footer */}
-        <footer className='flex flex-col py-8 w-full px-[8%] items-center'>
-          <div className='flex justify-center items-center gap-4 font-bold my-4 md:flex-row flex-col'>
+        <footer className='flex flex-col py-4 w-full px-[8%] items-center'>
+          <div className='flex justify-center items-center gap-4 font-semibold my-2 text-sm md:flex-row flex-col'>
             <div>Condition of Use</div>
             <div>Privacy & Policy</div>
             <div>Press Room</div>
           </div>
-            <div>&copy; 2023 MovieBox by Ibiyemi Samuel</div>
+            <div className='text-sm'>&copy; 2023 MovieBox by Ibiyemi Samuel</div>
         </footer>
     </div>
   )
