1843d9105d6738abc3d353eaac49f4b133952cd5
sam-dude <blendert5@gmail.com>
2023-09-15 01:50:08 +0100

    fix

--- src/routes/MovieDetail.jsx
+++ src/routes/MovieDetail.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useLoaderData, useParams } from 'react-router-dom'
+
+export default function MovieDetail() {
+    
     const res = useLoaderData()
     console.log(res)
   return (
