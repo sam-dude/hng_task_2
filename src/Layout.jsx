1d93921605f5c3589cfd525cf01c30d13f5b1881
sam-dude <blendert5@gmail.com>
2023-09-15 20:31:49 +0100

    fix

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
         <Outlet />
         {/* Footer */}
         <footer className='flex flex-col py-4 w-full px-[8%] items-center'>
-          <div className='flex justify-center items-center gap-4 font-semibold my-2 text-sm md:flex-row flex-col'>
+          <div className='flex justify-center items-center md:gap-4 gap-2 font-semibold my-2 text-sm md:flex-row flex-col'>
             <div>Condition of Use</div>
             <div>Privacy & Policy</div>
             <div>Press Room</div>
